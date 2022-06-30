328195191e35df49e21e0b5392e77a952506f90c
ght <573955230@qq.com>
2022-07-01 00:41:03 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     var now = "tab1";
+//     $(".tab").click(function(){
+//         var index = $(this).attr("name");
+//         if(index!=now){
+//             console.log(now);
+//             console.log(index);
+//             $("."+now).removeClass("line");
+//             $("."+index).addClass("line");
+
+            
+//             $("."+now + "-content").addClass("none");
+//             $("."+index + "-content").removeClass("none");
+            
+//             now = index;
+//         }
+//     })
+// });
+ 
